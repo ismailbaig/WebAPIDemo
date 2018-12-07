9c2289d7d0489d804ba82802bbb2f753f9f42a09
ismail1986 <ismailece2007@gmail.com>
2018-12-07 19:31:17 +0530

    Post method added

--- MS.MVCDemo/Controllers/api/StudentApiController.cs
+++ MS.MVCDemo/Controllers/api/StudentApiController.cs
@@ -26,5 +26,26 @@ namespace MS.MVCDemo.Controllers.api
 
             return Ok(studentVM);
         }
+
+        //Get action methods of the previous section
+        public IHttpActionResult PostNewStudent(StudentViewModel student)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            //using (var ctx = new SchoolDBEntities())
+            //{
+            //    ctx.Students.Add(new Student()
+            //    {
+            //        StudentID = student.Id,
+            //        FirstName = student.FirstName,
+            //        LastName = student.LastName
+            //    });
+
+            //    ctx.SaveChanges();
+            //}
+
+            return Ok();
+        }
     }
 }
